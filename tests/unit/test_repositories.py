9fb6f08050496f17a8fd4051b756369207861483
Smieluuu <kacper.smielak@wp.pl>
2023-06-07 09:54:11 +0200

    Update test_repositories.py

--- tests/unit/test_repositories.py
+++ tests/unit/test_repositories.py
@@ -1,54 +1,33 @@
 import pytest
-
 from eat_it.repositories import UserRepository
-
 
 @pytest.fixture
 def repository() -> UserRepository:
     return UserRepository()
 
-
-def test_can_instantiate_user_repository(
-    repository: UserRepository,
-) -> None:
-    pass
-
+def test_can_instantiate_user_repository(repository: UserRepository) -> None:
+    assert isinstance(repository, UserRepository)
 
 def test_raises_on_add_method(repository: UserRepository) -> None:
     with pytest.raises(NotImplementedError):
         repository.add()
 
+def test_raises_on_get_method(repository: UserRepository) -> None:
+    with pytest.raises(NotImplementedError):
+        repository.get()
 
-# PRACA DOMOWA 
-# 1. GET /users => zwraca 501
+def test_raises_on_delete_method(repository: UserRepository) -> None:
+    with pytest.raises(NotImplementedError):
+        repository.delete()
 
-# def test_get_users(repository: UserRepository) -> None:
-#     with pytest.raises(NotImplementedError):
-#         repository.get()
+def test_raises_on_post_method(repository: UserRepository) -> None:
+    with pytest.raises(NotImplementedError):
+        repository.post()
 
+def test_raises_on_put_method(repository: UserRepository) -> None:
+    with pytest.raises(NotImplementedError):
+        repository.put()
 
-# # 2. POST /users => zwraca przesłanego JSON-a i kod odpowiedzi 201
-
-# def test_post_users(repository: UserRepository) -> None:
-#     with pytest.raises(NotImplementedError):
-#         repository.post()
-
-# # 3. PUT /users/<id> => zwraca przesłanego JSON-a i kod odpowiedzi 200
-
-# def test_put_users(repository: UserRepository) -> None:
-#     with pytest.raises(NotImplementedError):
-#         repository.put()
-
-# # 4. PATCH /users/<id> => zwraca przesłanego JSON-a i kod odpowiedzi 200
-
-# def test_patch_users(repository: UserRepository) -> None:
-#     with pytest.raises(NotImplementedError):
-#         repository.patch()
-
-# # 5. DELETE /users/<id> => zwraca 204
-
-# def test_delete_users(repository: UserRepository) -> None:
-#     with pytest.raises(NotImplementedError):
-#         repository.delete()
-
-        
+def test_raises_on_patch_method(repository: UserRepository) -> None:
+    with pytest.raises(NotImplementedError):
+        repository.patch()
